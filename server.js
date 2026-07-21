2644c160abba03c73980ed1d89f148c0faa44bf5
NgocHuy89 <huynnhe173359@fpt.edu.vn>
2026-07-21 18:19:39 +0700

    server git

---
+++ server.js
@@ -0,0 +1,15 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const path = require('path');
+const router = jsonServer.router(path.join(__dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
+
+// Render will provide the port through the PORT environment variable
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server is running on port ${port}`);
+});
